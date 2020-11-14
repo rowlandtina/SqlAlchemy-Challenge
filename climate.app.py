670cffc6a1535d6f9e5aef0b7f7d15c6b1b363c9
Tina Rowland <cyd17gentco@gmail.com>
2020-11-14 09:45:48 -0500

    fix error

---
+++ climate.app.py
@@ -0,0 +1,32 @@
+%matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+measurement = Base.classes.measurement
+station = Base.classes.station
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
